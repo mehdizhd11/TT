6a31ce92e2bd6a883d3f2107d5be3981fa89fd95
MeT <mhdzahedi9936@gmail.com>
2023-10-14 21:44:47 +0330

    Fast-API

--- main.py
+++ main.py
@@ -1,58 +1,62 @@
-import requests
+from fastapi import FastAPI
 import sqlite3
 from bs4 import BeautifulSoup
-import os
+import requests
 
-db_path = 'website_data.db'
+app = FastAPI()
 
-os.remove(db_path)
+@app.get("/")
+def scrape_and_store_data():
+    conn = sqlite3.connect('website_data.db')
+    cursor = conn.cursor()
 
-conn = sqlite3.connect('website_data.db')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS website_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            price TEXT,
+            change TEXT
+        )
+    ''')
 
-cursor = conn.cursor()
+    url = 'https://www.tgju.org/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS website_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        price TEXT,
-        change TEXT
-    )
-''')
+    ul_elements = soup.find('ul', class_='info-bar')
+    li_elements = ul_elements.find_all('li')
 
-url = 'https://www.tgju.org/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+    h3_tags = []
+    for li in li_elements:
+        h3_tags.append(li.find('h3'))
+    titles = [element.text.strip() for element in h3_tags]
 
-ul_elements = soup.find('ul', class_='info-bar')
-li_elements = ul_elements.find_all('li')
+    info_price_elements = soup.find_all(class_='info-price')
+    prices = [element.text.strip() for element in info_price_elements]
 
-h3_tags = []
-for li in li_elements:
-    h3_tags.append(li.find('h3'))
+    change_elements = soup.find_all('span', class_='info-change')
+    changes = [element.text.strip() for element in change_elements]
 
-titles = [element.text.strip() for element in h3_tags]
+    # Inserting the data into the table
+    json = []
+    for index in range(9):
+        
+        title = titles[index]
+        price = prices[index]
+        change = changes[index]
+        
+        json.append(
+            {
+                'title': titles[index],
+                'price': prices[index],
+                'change': changes[index]
+            }
+        )
+        
+        cursor.execute('INSERT INTO website_data (title, price, change) VALUES (?, ?, ?)', (title, price, change))
+        conn.commit()
 
-info_price_elements = soup.find_all(class_='info-price')
-prices = [element.text.strip() for element in info_price_elements]
+    # Closing the connection
+    conn.close()
 
-change_elements = soup.find_all('span', class_='info-change')
-changes = [element.text.strip() for element in change_elements]
-
-# Inserting the data into the table
-for index in range(9):
-    title = titles[index]
-    price = prices[index]
-    change = changes[index]
-    cursor.execute('INSERT INTO website_data (title, price, change) VALUES (?, ?, ?)', (title, price, change))
-    conn.commit()
-
-    # print(f'{titles[index]} / {prices[index]} / {changes[index]}')
-
-cursor.execute("SELECT * FROM website_data ")
-
-rows = cursor.fetchall()
-
-for row in rows:
-    
-    print(row)
+    return json
